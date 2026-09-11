2960f57daeea7b318f7123dfb273d337b6167303
Mopara Pair Ayat <Moparapairayat@gmail.com>
2026-09-11 08:35:44 +0300

    fix dep. Error

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: ["@nexora/config", "@nexora/types"],
 };
 
 export default nextConfig;
